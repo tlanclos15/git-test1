96be5f129d987a509f5e0c693c9291447a2473a5
tlanclos <tlanclos@uncc.edu>
2020-09-23 19:18:47 -0400

    week03 commit

---
+++ backend/my-server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+http.createServer(function (req, res){
+    res.write('Hello World from Tafari');
+    res.end();
+}).listen(3000, function(){
+    console.log('Server running at port 3000');
+});
