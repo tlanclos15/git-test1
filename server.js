02885768a543864c287f469b610fb8a3ec9baa09
tlanclos <tlanclos@uncc.edu>
2020-10-12 21:11:13 -0400

    corrected chart size

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use(express.static('/public'));
+//app.use(express.static('/public'));
 app.use(cors());
 
 const budget = {
@@ -41,9 +41,9 @@ const budget = {
 ]
 };
 
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
-});
+//app.get('/hello', (req, res) => {
+//    res.send('Hello World!');
+//});
 
 app.get('/budget', (req, res) => {
     res.json(budget);
